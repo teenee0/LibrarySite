88998c6e1a9cb3adc9dcb55f443cd3bbfdf02466
teenee0 <qwerty.gogi@mail.ru>
2024-09-14 18:55:22 +0500

    1

---
+++ src/main/java/com/example/librarysite/LibrarysiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarysiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarysiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/librarysite/LibrarysiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
